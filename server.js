b8a6e8e1e25edabdf986809c1335b5679d009dfa
utilrr <gmarkghealy@gmail.com>
2022-04-08 20:55:33 -0400

    Not sure what changed

--- server.js
+++ server.js
@@ -5,10 +5,27 @@ const axios = require("axios");
 const apiUrl = "https://randommer.io/api/Name?nameType=fullname&quantity=20";
 
 console.log("token", process.env.RANDOMMER_API_TOKEN);
+// console.log("token", "d6946b6c4b86405aa4b74834771e5bff");
 
 app.get("/", (req, res) => {
   res.send("HELLO API");
 });
+
+// app.get("/names", async (req, res, next) => {
+//   try {
+//     if (!process.env.RANDOMMER_API_TOKEN) {
+//       throw new Error("YOU FAILED TO SET RANDOMMER_API_TOKEN");
+//     }
+//     const result = await axios.get(apiUrl, {
+//       headers: {
+//         "X-Api-Key": process.env.RANDOMMER_API_TOKEN,
+//       },
+//     });
+//     res.json(result.data);
+//   } catch (err) {
+//     next(err);
+//   }
+// });
 
 app.get("/names", async (req, res, next) => {
   const result = await axios.get(apiUrl, {
